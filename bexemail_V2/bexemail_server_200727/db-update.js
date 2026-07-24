3310f2dcbcedadc4c06cd5fe2142f6010478648b
chavdavimaln <chavdavimaln@gmail.com>
2026-07-24 20:12:20 +0530

    update contact

--- bexemail_V2/bexemail_server_200727/db-update.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('./node_modules/mysql2/promise');
-async function run() {
-  const c = await mysql.createConnection({host: '127.0.0.1', user: 'root', password: '', database: 'db_bex_email'});
-  await c.query("UPDATE lists SET name = 'Subscribers Directory' WHERE id = 1");
-  console.log('Done');
-  c.end();
-}
-run();
